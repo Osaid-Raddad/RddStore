ddf8627f20c003327d9e24a16bdb286ab1519ee0
Osaid-Raddad <osaidislam1@gmail.com>
2025-10-19 16:48:05 +0300

    Add Security Model

---
+++ RddStore.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RddStore.DAL.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
