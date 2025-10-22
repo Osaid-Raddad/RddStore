9906e5457841e3a0cf743307c51a43e704c5cfcd
Osaid-Raddad <osaidislam1@gmail.com>
2025-10-22 15:52:41 +0300

    Add Identity (register,login)

---
+++ RddStore.BLL/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using RddStore.DAL.DTO.Requests;
+using RddStore.DAL.DTO.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RddStore.BLL.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<UserResponse> RegisterAsync(RegisterRequest registerRequest);
+        Task<UserResponse> LoginAsync(LoginRequest loginRequest);
+    }
+}
